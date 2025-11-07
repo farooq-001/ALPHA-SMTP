b349b3bc6046510a01cd5f56c2fea0927cc8a75c
SUNKESULA NELAMPATI BABA FAROOQ <107756656+farooq-001@users.noreply.github.com>
2025-11-07 11:36:28 +0530

    Update system-monitor-seup.sh

--- system-monitor-seup.sh
+++ system-monitor-seup.sh
@@ -10,6 +10,7 @@ services:
     build:
       context: .
       dockerfile: Dockerfile
+#   image: baba001/system-monitor-smtp:v1
     image: baba001/system-monitor-smtp:v2
     container_name: system-monitor-container
     volumes:
